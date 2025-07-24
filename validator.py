f9639cd0d356177b2e4c3142e22b6d1470b38d31
Hadriely Harrizon Silva <hadrielyharrizon3@gmail.com>
2025-07-24 15:14:12 +0000

    Add CPF and CNPJ validation with formatting option

--- validator.py
+++ validator.py
@@ -6,19 +6,51 @@ based on the official rules for verifying digits.
 
 Author: Hadriely Harrizon
 '''
-
-print ("Validação de CPF/CNPJ");
-doc_type = input ("CPF - 1 | CNPJ - 2: ");
-
-if doc_type == "1":
-    doc = input("Digite o CPF: ")
-elif doc_type == "2":
-    doc = input("Digite o CNPJ: ")
-else:
-    print("Opção Inválida.")
-
 def clean_doc(doc):
     return ''.join(filter(str.isdigit, doc))
 
-documento_limpo = clean_doc(doc)
-print("Documento limpo:", documento_limpo)
+def formatcpf(cpf):
+    return f"{cpf[0:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:11]}"
+
+def formatcnpj(cnpj):
+    return f"{cnpj[0:2]}.{cnpj[2:5]}.{cnpj[5:8]}/{cnpj[8:12]}-{cnpj[12:14]}"
+
+
+while True:
+
+    print ("Validação de CPF/CNPJ");
+    doc_type = input ("CPF - 1 | CNPJ - 2: ");
+
+    if doc_type == "1":
+        doc = input("Digite o CPF: ")
+        documento_limpo = clean_doc(doc)
+        formattedcpf = input("Deseja seu CPF formatado? (s/n) ").strip().lower()
+
+        if formattedcpf == "s":
+            print('CPF formatado: ', formatcpf(documento_limpo))
+        elif formattedcpf == "n":
+            print('CPF limpo:', documento_limpo)
+        else:
+            print('Opção inválida.')
+
+
+    elif doc_type == "2":
+        doc = input("Digite o CNPJ: ")
+        documento_limpo = clean_doc(doc)
+        formattedcnpj = input('Deseja seu CNPJ formatado? (s/n)').strip().lower()
+
+        if formattedcnpj == "s":
+            print('CNPJ formatado:', formatcnpj(documento_limpo))
+        elif formattedcnpj == 'n':
+            print("CNPJ limpo:", documento_limpo)
+        else:
+            print('Opção inválida.')
+
+    else:
+        print("Opção Inválida.")
+
+    continuar = input("Deseja validar outro documento? (s/n) ")
+    if continuar == "n":
+        print("Encerrando...")
+        break
+
