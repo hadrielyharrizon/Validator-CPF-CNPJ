f945f97ffd2ee1648cdeacde56f9eb51aeb9685e
Hadriely Harrizon Silva <hadrielyharrizon3@gmail.com>
2025-07-24 03:12:14 +0000

    Add user input and document cleaning function for CPF/CNPJ

--- validator.py
+++ validator.py
@@ -6,3 +6,19 @@ based on the official rules for verifying digits.
 
 Author: Hadriely Harrizon
 '''
+
+print ("Validação de CPF/CNPJ");
+doc_type = input ("CPF - 1 | CNPJ - 2: ");
+
+if doc_type == "1":
+    doc = input("Digite o CPF: ")
+elif doc_type == "2":
+    doc = input("Digite o CNPJ: ")
+else:
+    print("Opção Inválida.")
+
+def clean_doc(doc):
+    return ''.join(filter(str.isdigit, doc))
+
+documento_limpo = clean_doc(doc)
+print("Documento limpo:", documento_limpo)
